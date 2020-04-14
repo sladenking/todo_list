8fd00580c75d1c6c68681ef23ad13f413646b41d
Viacheslav Denysko <deniskosv@gmail.com>
2020-04-14 21:25:31 +0300

    trying PWA

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+let express = require('express'),
+	app = express();	
+app.set('port', 8080);
+app.use(express.static(__dirname));
+app.listen(app.get('port'), function() {
+    console.log('Server started: http://localhost:' + app.get('port') + '/');
+});
